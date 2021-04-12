c80b5a3df4bc7b8b14122deda09a688600c69e55
mattr92 <80565694+mattr92@users.noreply.github.com>
2021-04-11 22:00:55 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 // variables for password
-const empty = "";
-const upper = "ABCDEFGHIJKLMNOPQRSTUVWUXYZ";
-const lower = "abcdefghijklmnopqrstuvwxyz";
-const nums = "0123456789";
-const symb = "!@#$%^&*";
+var blank = "";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWUXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var nums = "0123456789";
+var symb = "!@#$%^&*";
 //variables to call the characters from html
-const pwlength = document.getElementById("pw-length");
-const pwupper = document.getElementById("pw-upper");
-const pwlower = document.getElementById("pw-lower");
-const pnumb = document.getElementById("pw-numbers");
-const symbols = document.getElementById("pw-symbols");
-const generate = document.getElementById("generate");
-const password = document.getElementById("password");
+const pwlength = 
+document.getElementById("pw-length");
+const pwupper = 
+document.getElementById("pw-upper");
+const pwlower = 
+document.getElementById("pw-lower");
+const pnumb = 
+document.getElementById("pw-numbers");
+const symbols = 
+document.getElementById("pw-symbols");
+const generate = 
+document.getElementById("generate");
+const password = 
+document.getElementById("password");
 
 
 
@@ -19,22 +26,22 @@ const password = document.getElementById("password");
 functiom. If boxes are checked will return value in generated
  password*/
 generate.addEventListener("click", () => {
-    let initialPassword = empty;
-    (pwupper.checked) ? initialPassword += upper : "";
-    (pwlower.checked) ? initialPassword += lower : "";
-    (pnumb.checked) ? initialPassword += nums : "";
-    (symbols.checked) ? initialPassword += symb : "";
+    let blankpassword = blank;
+    (pwupper.checked) ? blankpassword += upper : "";
+    (pwlower.checked) ? blankpassword += lower : "";
+    (pnumb.checked) ? blankpassword += nums : "";
+    (symbols.checked) ? blankpassword += symb : "";
 
     password.value = generatePassword(pwlength.value,
-        initialPassword)
+        blankpassword)
 });
 //Generate button press will generate password
-function generatePassword(l, initialPassword) {
+function generatePassword(g, blankpassword) {
     let pass = "";
-    for (let i = 0; i < l; i++) {
+    for (let i = 0; i < g; i++) {
     //will return the characters that are called upon randomly
-        pass += initialPassword.charAt(Math.floor(Math.
-        random() * initialPassword.length));
+        pass += blankpassword.charAt(Math.floor(Math.
+        random() * blankpassword.length));
     }
     return pass;
 }
